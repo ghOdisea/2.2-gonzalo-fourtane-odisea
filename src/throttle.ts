4a3007b4b277020686c3adeaefb19c5eb2b0c822
ghOdisea <154799410+ghOdisea@users.noreply.github.com>
2024-03-22 21:05:36 +0100

    comentarios agregados

--- src/throttle.ts
+++ src/throttle.ts
@@ -1,16 +1,16 @@
 export default function throttle(funcion : Function, ms = 1000){ 
+    // Devuelve una funcion con los argumentos a recibir, 
     return (args: any) => {
-
-        let shouldWait = true
+    //Creo una variable que no puede entrar en el if, asique pasa a llamar a la funcion.
+        let shouldWait = false
         if ( shouldWait ) return 
-        
         //Llamo a la funcion
         funcion(args)
         shouldWait = true
         //Una vez que invoco la funcion se queda en true hasta que pase el tiempo.
-
         setTimeout(() => {
             shouldWait = false
         }, ms)
+        //Esto ejecuta la funcion una vez, y hasta que no pase el tiempo, no puede volver a ejecutarse.
     }
 }
